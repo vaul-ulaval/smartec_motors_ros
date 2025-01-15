3d04b9e9d5271728418e8d57dc2b5211e5e82f95
weasel <vaul@fsg.ulaval.ca>
2025-01-14 23:14:49 -0500

    odom and status working

--- smartec_motor_driver/launch/smartec_motor_driver.launch.py
+++ smartec_motor_driver/launch/smartec_motor_driver.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess
 import os
 
 
@@ -12,6 +13,13 @@ def generate_launch_description():
     # Load config files
     config = os.path.join(share_path, "config", "smartec_motor_driver.yaml")
     db_file = os.path.join(share_path, "config", "smartec_canbus.dbc")
+
+    # init_canbus = ExecuteProcess(
+    #     name="init_can",
+    #     cmd=["sudo ip link set can32 up type can bitrate 500000"],
+    #     shell=True
+    # )
+    # ld.add_action(init_canbus)
 
     smartec_motor_driver = Node(
         package='smartec_motor_driver',
--- smartec_motor_driver/scripts/smartec_motor_driver.py
+++ smartec_motor_driver/scripts/smartec_motor_driver.py
@@ -2,106 +2,173 @@
 
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import ReentrantCallbackGroup
 from std_msgs.msg import Float32
 from smartec_msgs.msg import SmartecStatus
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 import can
 import cantools
 import math
-
 
 class SmartecDriver(Node, can.Listener):
 
     def __init__(self):
         super().__init__('smartec_motor_driver')
 
+        self.last_cmd_time = self.get_clock().now()
+        self.last_odom_time = self.get_clock().now()
+        self.left_status = SmartecStatus()
+        self.right_status = SmartecStatus()
+        self.pose = [0, 0, 0]
+
+        self.setup_parameters()
+        self.init_subscribers()
+        self.init_publishers()
+        self.init_timers()    
+        self.init_canbus()
+
+        self.stop_motors()
+
+    def setup_parameters(self):
         self.interface = self.declare_parameter('interface', 'socketcan').value
         self.channel = self.declare_parameter('channel', 'can32').value
         self.bitrate = self.declare_parameter('bitrate', 500000).value
         self.database_file = self.declare_parameter('database_file', '').value
-        self.command_rate = self.declare_parameter('command_rate', 20).value
-        self.status_rate = self.declare_parameter('status_rate', 50).value
+        self.command_rate = self.declare_parameter('command_rate', 50).value
+        self.status_rate = self.declare_parameter('status_rate', 20).value
+        self.odom_rate = self.declare_parameter('odom_rate', 50).value
         self.base_width = self.declare_parameter('base_width', 1.0).value
         self.wheel_radius = self.declare_parameter('wheel_radius', 0.2).value
+        self.gear_ratio = self.declare_parameter('gear_ratio', 17.37).value
+        self.idle_timeout = self.declare_parameter('idle_timeout', 0.5).value
         self.debug = self.declare_parameter('debug', False).value
 
-        self.left_vel_sub = self.create_subscription(Float32, '/motors/left/velocity', self.vel_left_callback, 10)
-        self.right_vel_sub = self.create_subscription(Float32, '/motors/right/velocity', self.vel_right_callback, 10)
+    def init_subscribers(self):
         self.twist_sub = self.create_subscription(Twist, '/motors/cmd_vel', self.twist_callback, 10)
-        self.left_velocity = 0.0
-        self.right_velocity = 0.0
 
+    def init_publishers(self):
         self.left_status_pub = self.create_publisher(SmartecStatus, '/motors/left/status', 10)
         self.right_status_pub = self.create_publisher(SmartecStatus, '/motors/right/status', 10)
-        self.left_status = SmartecStatus()
-        self.right_status = SmartecStatus()
+        self.odometry_pub = self.create_publisher(Odometry, '/motors/odom', 10)
 
+    def init_timers(self):
+        self.cmd_timer = self.create_timer(1 / self.command_rate, self.send_commands)
+        self.idle_timer = self.create_timer(1 / self.command_rate, self.check_idle)
+        self.status_timer = self.create_timer(1 / self.status_rate, self.publish_status)
+        self.odom_timer = self.create_timer(1 / self.odom_rate, self.publish_odometry)
+
+    def init_canbus(self):
         self.can_bus = can.interface.Bus(self.channel, interface=self.interface, bitrate=self.bitrate)
         self.database = cantools.database.load_file(self.database_file)
         self.accepted_ids = [msg.frame_id for msg in self.database.messages]
-
-        self.notifier = can.Notifier(self.can_bus, [self])   # Calls on_message_received when a message is received
-        self.cmd_timer = self.create_timer(1/self.command_rate, self.send_commands)
-        self.status_timer = self.create_timer(1/self.status_rate, self.publish_messages)
-        
-    def vel_left_callback(self, msg):
-        self.left_velocity = int(msg.data / (2 * math.pi) * 60)  # Convert rad/s to RPM
-
-    def vel_right_callback(self, msg):
-        self.right_velocity = int(msg.data / (2 * math.pi) * 60) # Convert rad/s to RPM
+        self.notifier = can.Notifier(self.can_bus, [self])
 
     def twist_callback(self, msg):
         # Convert linear and angular velocities to left and right wheel velocities
-        linear = msg.linear.x
-        angular = msg.angular.z
-        left_vel_rads = int((linear - angular * self.base_width / 2) / self.wheel_radius)
-        right_vel_rads = int((linear + angular * self.base_width / 2) / self.wheel_radius)
-        self.left_velocity = int(left_vel_rads / (2 * math.pi) * 60)  # Convert rad/s to RPM
-        self.right_velocity = int(right_vel_rads / (2 * math.pi) * 60)  # Convert rad/s to RPM
+        self.last_cmd_time = self.get_clock().now()
+        left_cmd_rad = (msg.linear.x - msg.angular.z * self.base_width / 2) / self.wheel_radius * self.gear_ratio
+        right_cmd_rad = (msg.linear.x + msg.angular.z * self.base_width / 2) / self.wheel_radius * self.gear_ratio
+        self.left_command = int(left_cmd_rad / (2 * math.pi) * 60)  # Convert rad/s to RPM
+        self.right_command = int(right_cmd_rad / (2 * math.pi) * 60)  # Convert rad/s to RPM
 
     def on_message_received(self, msg):
         # Handle received CAN messages
         if msg.arbitration_id in self.accepted_ids:
-            decoded_message = self.database.decode_message(msg.arbitration_id, msg.data)
-            if decoded_message.name == 'Left_GDM_General_Status':
+            msg_name = self.database.get_message_by_frame_id(msg.arbitration_id).name
+            # self.get_logger().info("ALLO1")
+            if msg_name == 'Left_GDM_General_Status':
+                decoded_message = self.database.decode_message(msg.arbitration_id, msg.data)
                 self.update_status(decoded_message, self.left_status)
-            elif decoded_message.name == 'Right_GDM_General_Status':
+            elif msg_name == 'Right_GDM_General_Status':
+                decoded_message = self.database.decode_message(msg.arbitration_id, msg.data)
                 self.update_status(decoded_message, self.right_status)
 
     def send_commands(self):
         # Send velocity commands to the motors
-        command = self.database.encode_message('Control_GDM_Left_Right', {
-            'Left_ControlMode': 2,
-            'Left_Command': self.left_velocity,
-            'Right_ControlMode': 2,
-            'Right_Command': self.right_velocity
+        message = self.database.get_message_by_name('Control_GDM_Left_Right')
+        command = message.encode({
+            'Left_ControlMode': 2 if self.left_command != 0 else 1,
+            'Left_Command': self.left_command,
+            'Right_ControlMode': 2 if self.right_command != 0 else 1,
+            'Right_Command': self.right_command
         })
-        self.can_bus.send(command)
+        try:
+            self.get_logger().info(f"Sending command to motors: Left {self.left_command}, Right {self.right_command}")
+            self.can_bus.send(can.Message(arbitration_id=message.frame_id, data=command))
+        except can.CanOperationError as e:
+            self.get_logger().error(f"Failed to transmit: {e}. Retrying...")
 
-    def publish_messages(self): 
+    def publish_status(self): 
         self.left_status_pub.publish(self.left_status)
         self.right_status_pub.publish(self.right_status)
 
     def update_status(self, decoded_message, status):
         status.header.stamp = self.get_clock().now().to_msg()
         status.header.frame_id = 'base_link'
-        status.mode = decoded_message["ControlMode"]
-        status.fault_code = decoded_message["LatchedFaultCode"]
-        status.speed = decoded_message["MeasuredRPM"] * 2.0 * math.pi / 60.0
-        status.voltage = decoded_message["BusVoltage"]
-        status.current = decoded_message["AlignedTorqueCurrent"]
-        status.power = decoded_message["EstimatedPower"]
+        status.mode = int(decoded_message["ControlMode"])
+        status.fault_code = int(decoded_message["LatchedFaultCode"])
+        status.speed = float(decoded_message["MeasuredRPM"] * 2 * math.pi / 60.0 / self.gear_ratio)
+        status.voltage = float(decoded_message["BusVoltage"])
+        status.current = float(decoded_message["AlignedTorqueCurrent"])
+        status.power = float(decoded_message["EstimatedPower"])
+
+    def publish_odometry(self):
+        current_time = self.get_clock().now()
+        dt = (current_time - self.last_odom_time).nanoseconds * 1e-9  # Convert ns to seconds
+        self.last_odom_time = current_time
+
+        odom_msg = Odometry()
+        odom_msg.header.stamp = current_time.to_msg()
+        odom_msg.header.frame_id = 'odom'
+        odom_msg.child_frame_id = 'base_link'
+
+        # Calculate linear and angular velocities
+        linear_velocity_x = self.wheel_radius * (self.right_status.speed + self.left_status.speed) / 2.0
+        angular_velocity_z = self.wheel_radius * (self.right_status.speed - self.left_status.speed) / self.base_width
+        odom_msg.twist.twist.linear.x = linear_velocity_x
+        odom_msg.twist.twist.angular.z = angular_velocity_z
+
+        # Update the robot's pose
+        self.pose[2] += odom_msg.twist.twist.angular.z * dt
+        self.pose[0] += odom_msg.twist.twist.linear.x * dt * math.cos(self.pose[2])
+        self.pose[1] += odom_msg.twist.twist.linear.x * dt * math.sin(self.pose[2])
+
+        # Populate the Pose message
+        odom_msg.pose.pose.position.x = self.pose[0]
+        odom_msg.pose.pose.position.y = self.pose[1]
+        quaternion = self.euler_to_quaternion(0.0, 0.0, self.pose[2])
+        odom_msg.pose.pose.orientation.x = quaternion[0]
+        odom_msg.pose.pose.orientation.y = quaternion[1]
+        odom_msg.pose.pose.orientation.z = quaternion[2]
+        odom_msg.pose.pose.orientation.w = quaternion[3]
+
+        self.odometry_pub.publish(odom_msg)
+
+    def euler_to_quaternion(self, roll, pitch, yaw):
+        """
+        Convert Euler angles to a quaternion.
+        """
+        qx = math.sin(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) - math.cos(roll / 2) * math.sin(pitch / 2) * math.sin(yaw / 2)
+        qy = math.cos(roll / 2) * math.sin(pitch / 2) * math.cos(yaw / 2) + math.sin(roll / 2) * math.cos(pitch / 2) * math.sin(yaw / 2)
+        qz = math.cos(roll / 2) * math.cos(pitch / 2) * math.sin(yaw / 2) - math.sin(roll / 2) * math.sin(pitch / 2) * math.cos(yaw / 2)
+        qw = math.cos(roll / 2) * math.cos(pitch / 2) * math.cos(yaw / 2) + math.sin(roll / 2) * math.sin(pitch / 2) * math.sin(yaw / 2)
+        return [qx, qy, qz, qw]
+
+    def check_idle(self):
+        elapsed_time = (self.get_clock().now() - self.last_cmd_time).nanoseconds / 1e9 
+        if elapsed_time > self.idle_timeout and (self.left_command != 0 or self.right_command !=0):
+            self.stop_motors()
 
     def stop_motors(self):
-        self.left_velocity = 0
-        self.right_velocity = 0
+        self.left_command = 0
+        self.right_command = 0
         self.send_commands()
 
     def shutdown(self):
         self.stop_motors()
         self.notifier.stop()
         self.can_bus.shutdown()
-        super().shutdown()
 
 
 def main(args=None):
